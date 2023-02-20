b9c0d5c600a2e7c3cad93cabc3319c9cf042a514
songhoujin <songhoujin@mediinfo.com.cn>
2023-02-21 01:15:26 +0800

    主要学习 常用控件,高级控件共7个; 同时视频课程看到 第四章节 数据存储部分 键值存储 和 数据库存储已经看完明天照着 仿写一遍

---
+++ Demo/Demo/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickHyperlinkButton_Tapped(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://docs.microsoft.com/zh-cn/");
+
+
+        }
+
+        private void MyPopupButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Greetings!", "You have clicked me!", "OK");
+        }
+    }
+}
---
+++ Demo/Demo/Views/SecondPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+
+            SizeChanged += (sender, args) =>
+                VisualStateManager.GoToState(BoxStackLayout, Width > Height ? "Landscape" : "Portarit");
+            // 语言组织太啰嗦,直接写
+            //List<Poetry> poetryList = new List<Poetry>();  
+            //Poetry p;
+            //p = new Poetry();
+            //p.Name = "Name 1";
+            //p.Content = "Content 1";
+            //poetryList.Add(p);
+            var poetryList = new List<Poetry>
+            {
+                new Poetry {Name = "Name 1", Content = "Content 1"},
+                new Poetry {Name = "Name 2", Content = "Content 2"},
+                new Poetry {Name = "Name 3", Content = "Content 3"},
+            };
+
+            PoetryListView.ItemsSource = poetryList;
+        }
+
+        private void PoetryListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert("", ((Poetry)e.Item).Name, "OK");
+            //输出Debug测试
+            Debug.WriteLine(((Poetry)e.Item).Name);
+        }
+    }
+    /// <summary>
+    /// 诗,有作者和内容两个属性
+    /// </summary>
+    public class Poetry
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
