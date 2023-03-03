ec38cf354fc94f8c8d115b99a9f597c493ef5314
Rocky-houjinsong <rocky.houjinsong@outlook.com>
2023-03-03 14:15:12 +0800

    提交 页面之间跳转功能

---
+++ DataBaseTest/ToConnection/ToConnection/Services/PreferenceStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace ToConnection.Services
+{
+    public class PreferenceStorage : IPreferenceStorage
+    {
+        public void Set(string key, string value) =>
+            Preferences.Set(key, value);
+
+        public void Set(string key, int value) =>
+            Preferences.Set(key, value);
+
+
+        public string Get(string key, string defaultValue) =>
+            Preferences.Get(key, defaultValue);
+
+
+        public int Get(string key, int defaultValue) =>
+            Preferences.Get(key, defaultValue);
+
+    }
+}
---
+++ Demo/Demo/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Demo.Models;
+using GalaSoft.MvvmLight;
+
+namespace Demo.ViewModels
+{
+    /// <summary>
+    /// 诗词详情页ViewModel.
+    /// </summary>
+    public class DetailPageViewModel : ViewModelBase
+    {
+        //******************绑定属性
+
+        public Poetry Poetry
+        {
+            get => _poetry;
+            set => Set(nameof(Poetry), ref _poetry, value);
+        }
+        /// <summary>
+        /// 诗词.
+        /// </summary>
+        private Poetry _poetry;
+    }
+}
