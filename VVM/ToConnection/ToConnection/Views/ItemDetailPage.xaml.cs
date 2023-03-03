8160265fb0ea84121867bb6a5ca4286588e631a8
Rocky-houjinsong <rocky.houjinsong@outlook.com>
2023-03-03 09:14:37 +0800

    View 和 ViewModel 通过 ViewModelLocator 进行建立链接 ; 演示 链接 数据库功能

---
+++ VVM/ToConnection/ToConnection/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ToConnection.ViewModels;
+using Xamarin.Forms;
+
+namespace ToConnection.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
