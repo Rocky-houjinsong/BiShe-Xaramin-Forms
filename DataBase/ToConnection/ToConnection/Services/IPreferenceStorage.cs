159c4f058ceb17f1afd6d654b037fea4adb6cf83
Rocky-houjinsong <rocky.houjinsong@outlook.com>
2023-03-03 09:19:06 +0800

    本地数据库功能实现 ; 但 view 和ViewModel在后面的VVM中实现; 功能测试 在 DataBaseTest中

---
+++ DataBase/ToConnection/ToConnection/Services/IPreferenceStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToConnection.Services
+{
+    /// <summary>
+    ///偏好存储接口.
+    /// </summary>
+    public interface IPreferenceStorage
+    {
+        /// <param name="key">Preference key.</param>
+        /// <param name="value">Preference value.</param>
+        /// <summary>Sets a value for a given key.</summary>
+        void Set(string key, string value);
+
+        void Set(string key, int value);
+        /// <param name="key">Preference key.</param>
+        /// <param name="defaultValue">Default value to return if the key does not exist.</param>
+        /// <summary>Gets the value for a given key, or the default specified if the key does not exist.</summary>
+        /// <returns>Value for the given key, or the default if it does not exist.</returns>
+        string Get(string key, string defaultValue);
+
+        int Get(string key, int defaultValue);
+    }
+}
---
+++ DataBase/ToConnection/ToConnection/Services/PoetryStorage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using ToConnection.Models;
+
+namespace ToConnection.Services
+{
+    public class PoetryStorage : IPoetryStorage
+    {
+        //********私有变量
+        /// <summary>
+        /// 数据库文件名  
+        /// </summary>
+        private const string DbName = "poetrydb.sqlite3";
+        /// <summary>
+        /// 数据库文件路径
+        /// </summary>
+        public static readonly string PoetryDbPath =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder
+                    .LocalApplicationData), DbName);
+
+        /// <summary>
+        /// 打开数据库连接
+        /// 打开数据库连接
+        /// </summary>
+        private SQLiteAsyncConnection _connection;
+
+        private SQLiteAsyncConnection Connection => _connection ??
+                                                   (_connection = new SQLiteAsyncConnection(PoetryDbPath));
+        /// <summary>
+        /// 偏好存储.
+        /// </summary>
+        private IPreferenceStorage _preferenceStorage;
+        //****************继承方法
+        /// <summary>
+        /// 初始化诗词存储
+        /// </summary>
+        /// <returns></returns>
+        /// <returns></returns>
+        public async Task InitializeAsync()
+        { // 打开文件,传递路径 将需要关闭的初始化 扔到using中,文件操作 必须要关闭,using就是该效果
+            using (var dbFilesStream =
+                   new FileStream(PoetryDbPath, FileMode.Create))
+            // dbAssertStream 数据资源流
+            using (var dbAssertStream = Assembly.GetExecutingAssembly()
+                       .GetManifestResourceStream(DbName))
+            {
+                await dbAssertStream.CopyToAsync(dbFilesStream);// 将目标文件拷贝到来源文件
+            }
+
+            _preferenceStorage.Set(PoetryStorageConstants.VersionKey, PoetryStorageConstants.Version);
+        }
+
+        /// <summary>
+        /// 诗词存储是否已经初始化.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInitiallized() =>
+            _preferenceStorage.Get(PoetryStorageConstants.VersionKey, PoetryStorageConstants.DefaultVersion) ==
+            PoetryStorageConstants.Version;
+
+        /// <summary>
+        ///获取一个诗词
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Poetry> GetPoetryAsync(int id) =>
+            await Connection.Table<Poetry>()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+        public async Task<IList<Poetry>> GetPoetriesAsync(Expression<Func<Poetry, bool>> where, int skip, int take) =>
+            await Connection.Table<Poetry>().Where(where).Skip(skip).Take(take).ToListAsync();
+        //********* 公开方法
+        /// <summary>
+        /// 诗词存储.
+        /// </summary>
+        /// <param name="preferenceStorage">偏好存储</param>
+        public PoetryStorage(IPreferenceStorage preferenceStorage)
+        {
+            _preferenceStorage = preferenceStorage;
+        }
+        /// <summary>
+        /// 关闭诗词数据库.
+        /// </summary>
+        /// <returns></returns>
+        public async Task CloseAsync() => await Connection.CloseAsync();
+    }
+}
